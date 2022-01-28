e41c6b94bdd8bb92d6e7c5c01449fc8b7c5f2a48
Charles-Muia <Charles.K.Muia@gmail.com>
2022-01-28 10:29:13 +0300

    PHP Class Data Types

--- PHPClassIntro.php
+++ PHPClassIntro.php
@@ -9,13 +9,21 @@ echo "Class PHP";
 <!doctype html>
 <html lang="en">
 <head>
+    <style>
+        .P1{
+            text-align: center;
+            font-family: "Microsoft New Tai Lue";
+            font-variant-caps: all-petite-caps;
+        }
+
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Introduction To PHP </title>
 </head>
-<body>
+<body class="P1">
 
 <?php
 echo "Php is interesting.";  //echo how to use strings
