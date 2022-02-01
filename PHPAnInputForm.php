ad0f3e2109c318188efd525c8610b0ac33e7c219
Charles-Muia <Charles.K.Muia@gmail.com>
2022-02-01 23:03:45 +0300

    More About PHP Conditional Statements

--- PHPAnInputForm.php
+++ PHPAnInputForm.php
@@ -1,15 +1,16 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="PHPStyle.css">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP Input Form | if...else...elseif...Statements</title>
 </head>
 <body>
 
-<form method="post" action="DisplayConditionIf.php">
+<form method="post" action="PHPAnInputDisplayConditionIf.php">
     Please Enter Your Grade:
     <p><input type="text" name="GiveMarks"></p>
     <input type="submit" value="Click To Submit">
